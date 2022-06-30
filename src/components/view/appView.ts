c60676c478dbe5e1a637d1789a3a6de0c5f55d38
max-makk <90equals50@gmail.com>
2022-06-30 03:33:40 -0700

    feat: convert appView.js to typescript syntax

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,34 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
+import { Item } from './sources/sources';
+import { Article } from './news/news';
+
+export interface NewsList {
+    articles: Article[];
+    status: string;
+    totalResults: number;
+}
+
+export interface SourcesList {
+    sources: Item[];
+    status: string;
+}
+
 export class AppView {
+    news: News;
+    sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: NewsList) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: SourcesList) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
